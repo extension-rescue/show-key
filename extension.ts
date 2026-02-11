a199f0fa638f5c588fab42beb5a329e3ee959d57
Yusheng <yshngg@outlook.com>
2026-02-11 23:08:08 +0800

    fix: correct key display and removal logic
    - Move remove_all_children() outside loop to show all pressed keys
    - Replace complex copyWithin() with splice() for proper key removal
    - Remove unused imports and variables
    - Add empty stylesheet for future styling

--- extension.ts
+++ extension.ts
@@ -1,74 +1,139 @@
-import GLib from "gi://GLib";
-import Gio from "gi://Gio";
-import Meta from "gi://Meta";
-import Shell from "gi://Shell";
-import GObject from "gi://GObject";
-import * as Main from "resource:///org/gnome/shell/ui/main.js";
+import St from "gi://St";
+import Clutter from "gi://Clutter";
+
 import {
-  Extension,
-  gettext as _,
+    Extension,
+    gettext as _,
 } from "resource:///org/gnome/shell/extensions/extension.js";
-import {
-  QuickToggle,
-  SystemIndicator,
-} from "resource:///org/gnome/shell/ui/quickSettings.js";
+import * as PanelMenu from "resource:///org/gnome/shell/ui/panelMenu.js";
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
 
-const ExampleToggle = GObject.registerClass(
-  class ExampleToggle extends QuickToggle {
-    constructor() {
-      super({
-        title: _("Smile"),
-        iconName: "face-smile-symbolic",
-        toggleMode: true,
-      });
-    }
-  },
-);
+export default class ShowKeyExtension extends Extension {
+    _panelButton: PanelMenu.Button | null = null;
+    _container: St.BoxLayout | null = null;
+    _filterID: number | null = null;
 
-const ExampleIndicator = GObject.registerClass(
-  class ExampleIndicator extends SystemIndicator {
-    _indicator: any;
+    private _pressedKeys: number[] = [];
 
-    constructor() {
-      super();
-
-      this._indicator = this._addIndicator();
-      this._indicator.iconName = "face-smile-symbolic";
-
-      const toggle = new ExampleToggle();
-      toggle.bind_property(
-        "checked",
-        this._indicator,
-        "visible",
-        GObject.BindingFlags.SYNC_CREATE,
-      );
-      this.quickSettingsItems.push(toggle);
-    }
-  },
-);
-
-export default class TypeScriptTemplateExtension extends Extension {
-  gsettings?: Gio.Settings;
-  animationsEnabled: boolean = true;
-  _indicator?: InstanceType<typeof ExampleIndicator>;
-
-  enable() {
-    this.gsettings = this.getSettings();
-    this.animationsEnabled = this.gsettings.get_boolean("animate") ?? true;
-
-    this._indicator = new ExampleIndicator();
-    Main.panel.statusArea.quickSettings.addExternalIndicator(
-      this._indicator as any,
-    );
-  }
-
-  disable() {
-    if (this._indicator) {
-      this._indicator.quickSettingsItems.forEach((item: any) => item.destroy());
-      this._indicator.destroy();
-      this._indicator = undefined;
+    update() {
+        console.debug(this._pressedKeys);
+        this._container?.remove_all_children();
+        for (const keysym of this._pressedKeys) {
+            console.log(keysym);
+            const label = new St.Label({
+                text: this._keysymToLabel(keysym),
+                style_class: "key-style",
+            });
+            this._container?.add_child(label);
+        }
     }
 
-    this.gsettings = undefined;
-  }
+    _onClutterEvent(event: Clutter.Event): boolean {
+        const eventType = event.type();
+        switch (eventType) {
+            case Clutter.EventType.KEY_PRESS: {
+                const keysym = event.get_key_symbol();
+                if (keysym === null) break;
+                console.log("pressed key:", keysym);
+                this._pressedKeys.push(keysym);
+                this.update();
+                break;
+            }
+            case Clutter.EventType.KEY_RELEASE: {
+                const keysym = event.get_key_symbol();
+                if (keysym === null) break;
+                console.log("released key:", keysym);
+                const index = this._pressedKeys.lastIndexOf(keysym);
+                if (index < 0) break;
+                this._pressedKeys.splice(index, 1);
+                this.update();
+                break;
+            }
+            case Clutter.EventType.KEY_STATE: {
+                console.log("key state:", event.get_key_state());
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+
+        return Clutter.EVENT_PROPAGATE;
+    }
+
+    private _keysymToLabel(keysym: number): string {
+        if (keysym >= 32 && keysym <= 126) {
+            return String.fromCharCode(keysym);
+        }
+
+        switch (keysym) {
+            case Clutter.KEY_Return:
+                return "↵";
+            case Clutter.KEY_Escape:
+                return "Esc";
+            case Clutter.KEY_BackSpace:
+                return "⌫";
+            case Clutter.KEY_Tab:
+                return "Tab";
+            case Clutter.KEY_space:
+                return "Space";
+            case Clutter.KEY_Up:
+                return "↑";
+            case Clutter.KEY_Down:
+                return "↓";
+            case Clutter.KEY_Left:
+                return "←";
+            case Clutter.KEY_Right:
+                return "→";
+            case Clutter.KEY_Control_L:
+            case Clutter.KEY_Control_R:
+                return "Ctrl";
+            case Clutter.KEY_Shift_L:
+            case Clutter.KEY_Shift_R:
+                return "Shift";
+            case Clutter.KEY_Alt_L:
+            case Clutter.KEY_Alt_R:
+                return "Alt";
+            case Clutter.KEY_Super_L:
+            case Clutter.KEY_Super_R:
+                return "Super";
+        }
+
+        return `Key(${keysym})`;
+    }
+
+    enable() {
+        this._panelButton = new PanelMenu.Button(0.0, _("Any Key?"));
+        this._container = new St.BoxLayout({
+            x_align: Clutter.ActorAlign.END,
+            y_align: Clutter.ActorAlign.CENTER,
+            style_class: "showkeys-container",
+        });
+
+        if (this._container.layout_manager) {
+            this._container.layout_manager.spacing = 6;
+        }
+        this._panelButton.add_child(this._container);
+        Main.panel.addToStatusArea(this.uuid, this._panelButton);
+        this._filterID = Clutter.event_add_filter(
+            null,
+            this._onClutterEvent.bind(this),
+        );
+    }
+
+    disable() {
+        if (this._filterID) {
+            Clutter.event_remove_filter(this._filterID);
+            this._filterID = null;
+        }
+
+        if (this._panelButton) {
+            if (this._container) {
+                this._panelButton.remove_child(this._container);
+            }
+            Main.panel.remove_child(this._panelButton);
+            this._panelButton = null;
+            this._container = null;
+        }
+    }
 }
